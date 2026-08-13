5fdc8a66988071f9981d71bd64196802469c0e2f
luiscornejogalarza <luiscornejogalarza@gmail.com>
2026-08-12 22:27:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,87 @@
 import streamlit as st
+import numpy as np
+import libreria_funciones as lf
 
-st.title("Especializacion Python for Analytics")
+st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
-st.write("Elaborado por: Luis Cornejo")
+st.write("Elaborado por: Carlos Carrillo")
+
+st.image("Python_logo.png", width = 300)
+st.sidebar.image("DMC.png",width = 100)
+
+
+modulos = st.sidebar.selectbox ("Selecione un módulo", ["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
+
+if modulos == "Módulo Listas":
+  
+  st.write("Bienvenido al módulo Listas")
+  
+  valor_inicial = st.number_input("Ingrese el valor inicial")
+  valor_final = st.number_input("Ingrese el valor final")
+  
+  lista_numeros = list(range(int(valor_inicial), int(valor_final)))
+  st.write(lista_numeros)
+  
+elif modulos == "Módulo Arreglos":
+  
+  st.write("Bienvenido al módulo de Arreglos")
+
+  cantidad_elementos = st.slider("Selecione la cantidad de elementos de su arreglo", 1,100)
+  cantidad_arreglo= np.arange(cantidad_elementos)
+  st.write(cantidad_arreglo)
+
+
+  
+else:
+  
+  st.write("Bienvenido al módulo de Funciones")
+
+  capital_inicial = st.number_input("Capital inicial", min_value=0.0, value=1000.0)
+  tiempo_meses = st.number_input("Tiempo en meses", min_value=1, value=12)
+  tasa_porcentaje = st.number_input("Tasa de interés anual (%)", min_value=0.0, value=0.05)
+
+  resultado_interes_simple = lf.interes_simple(capital_inicial, tiempo_meses,tasa_porcentaje )
+  st.write(resultado_interes_simple)import streamlit as st
+import numpy as np
+import libreria_funciones as lf
+
+st.title("Especialización Python for Analytics")
+st.sidebar.title("Parámetros")
+st.write("Elaborado por: Carlos Carrillo")
+
+st.image("Python_logo.png", width = 300)
+st.sidebar.image("DMC.png",width = 100)
+
+
+modulos = st.sidebar.selectbox ("Selecione un módulo", ["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
+
+if modulos == "Módulo Listas":
+  
+  st.write("Bienvenido al módulo Listas")
+  
+  valor_inicial = st.number_input("Ingrese el valor inicial")
+  valor_final = st.number_input("Ingrese el valor final")
+  
+  lista_numeros = list(range(int(valor_inicial), int(valor_final)))
+  st.write(lista_numeros)
+  
+elif modulos == "Módulo Arreglos":
+  
+  st.write("Bienvenido al módulo de Arreglos")
+
+  cantidad_elementos = st.slider("Selecione la cantidad de elementos de su arreglo", 1,100)
+  cantidad_arreglo= np.arange(cantidad_elementos)
+  st.write(cantidad_arreglo)
+
+
+  
+else:
+  
+  st.write("Bienvenido al módulo de Funciones")
+
+  capital_inicial = st.number_input("Capital inicial", min_value=0.0, value=1000.0)
+  tiempo_meses = st.number_input("Tiempo en meses", min_value=1, value=12)
+  tasa_porcentaje = st.number_input("Tasa de interés anual (%)", min_value=0.0, value=0.05)
+
+  resultado_interes_simple = lf.interes_simple(capital_inicial, tiempo_meses,tasa_porcentaje )
+  st.write(resultado_interes_simple)
