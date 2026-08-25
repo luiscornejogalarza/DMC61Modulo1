28ddecd147239abdfb833ad52444d028798a2c7b
luiscornejogalarza <luiscornejogalarza@gmail.com>
2026-08-25 17:36:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ import numpy as np
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Luis Cornejo")
+
+st.image(Python_logo.png, width = 300)
+st.sidebar.image(DMC.png,width = 100)
