7c15f3a61a4ca77a60320bdf75752ee0e8e358c4
luiscornejogalarza <luiscornejogalarza@gmail.com>
2026-08-25 18:44:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,34 @@ if menu == "Home":
     st.markdown("Soy biólogo de formación, actualmente aplicando mis habilidades analíticas hacia la ingeniería de datos.")
     st.write("**Descripción:** Aplicación interactiva que integra variables, estructuras de datos, widgets, funciones, clases y lógica de programación en una interfaz interactiva.")
     st.write("**Tecnologías:** Python, Streamlit, Pandas, NumPy")
+
+#Ejercicio 1 :Flujo de caja
+
+elif menu == "Ejercicio 1":
+    st.header("Ejercicio 1 - Flujo de Caja")
+    st.write("Registra movimientos financieros.") #[cite: 1]
+    
+    concepto = st.text_input("Concepto") #[cite: 1]
+    tipo = st.selectbox("Tipo de movimiento", ["Ingreso", "Gasto"]) #[cite: 1]
+    valor = st.number_input("Valor", min_value=0.0) #[cite: 1]
+    
+    if st.button("Agregar Movimiento"): #[cite: 1]
+        st.session_state.caja.append({"Concepto": concepto, "Tipo": tipo, "Valor": valor})
+        st.success("Agregado") #[cite: 1]
+        
+    if st.session_state.caja:
+        df_caja = pd.DataFrame(st.session_state.caja)
+        st.dataframe(df_caja) #[cite: 1]
+        
+        ingresos = df_caja[df_caja['Tipo'] == 'Ingreso']['Valor'].sum()
+        gastos = df_caja[df_caja['Tipo'] == 'Gasto']['Valor'].sum()
+        saldo = ingresos - gastos
+        
+        st.metric("Total Ingresos", f"${ingresos}") #[cite: 1]
+        st.metric("Total Gastos", f"${gastos}") #[cite: 1]
+        st.metric("Saldo Final", f"${saldo}") #[cite: 1]
+        
+        if saldo >= 0:
+            st.success("Flujo a favor") #[cite: 1]
+        else:
+            st.error("Flujo en contra") #[cite: 1]
