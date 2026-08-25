5f4c0e650ac8f35dc7509bd112ae2f0efa5d51a9
luiscornejogalarza <luiscornejogalarza@gmail.com>
2026-08-25 18:13:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,11 @@ menu = st.sidebar.selectbox(
 #Home
 
 if menu == "Home":
-    st.title("Proyecto Aplicado en Streamlit") #[cite: 1]
-    st.subheader("Módulo 1 - Python Fundamentals") #[cite: 1]
+    st.title("Proyecto Aplicado en Streamlit")
+    st.subheader("Módulo 1 - Python Fundamentals")
     
-    st.write("**Estudiante:** Luis Marco Cornejo Galarza") #[cite: 1]
-    st.write("**Año:** 2026") #[cite: 1]
+    st.write("**Estudiante:** Luis Marco Cornejo Galarza")
+    st.write("**Año:** 2026")
     st.markdown("Soy biólogo de formación, actualmente aplicando mis habilidades analíticas hacia la ingeniería de datos.")
-    
-    st.write("**Descripción:** Aplicación interactiva que integra variables, estructuras de datos, control de flujo, funciones y POO.") #[cite: 1]
-    st.write("**Tecnologías:** Python, Streamlit, Pandas, NumPy") #[cite: 1]
+    st.write("**Descripción:** Aplicación interactiva que integra variables, estructuras de datos, widgets, funciones, clases y lógica de programación en una interfaz interactiva.")
+    st.write("**Tecnologías:** Python, Streamlit, Pandas, NumPy")
